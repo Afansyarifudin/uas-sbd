f0f02cd4a6615f974d11c476f12568758e671e79
nurafansyarifudin <68774609+Afansyarifudin@users.noreply.github.com>
2021-11-29 19:34:15 +0700

    Database

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASEURL', 'http://localhost/uas-sbd/public');
+
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'inventaris');
+
+
