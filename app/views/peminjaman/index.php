884aa63a8d3ee0feaffa5ed018c7f2ca608705b8
nurafansyarifudin <68774609+Afansyarifudin@users.noreply.github.com>
2021-12-01 06:22:14 +0700

    views

---
+++ app/views/peminjaman/index.php
@@ -0,0 +1,6 @@
+<div class="container-fluid">
+    <div class="d-sm-flex justify-content-between align-items-center mb-4">
+        <h3 class="text-dark mb-0">Peminjaman</h3>
+    </div>
+                    
+</div>
