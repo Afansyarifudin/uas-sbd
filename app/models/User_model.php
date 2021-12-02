330f04a457dc9c435022c876a369ac5d1e1b4fd6
nurafansyarifudin <68774609+Afansyarifudin@users.noreply.github.com>
2021-12-02 21:24:21 +0700

    update database

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -1,6 +1,12 @@
 <?php
 
-class User_model 
+class User_model
 {
-    
+    private $table = 'user';
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
 }
