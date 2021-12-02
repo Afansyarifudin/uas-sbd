36fbb7f81f72ba437b452eb6c3a4022247132c82
nurafansyarifudin <68774609+Afansyarifudin@users.noreply.github.com>
2021-12-02 12:32:16 +0700

    update database

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,6 +1,6 @@
 <?php
 
-class Database 
+class Database
 {
     private $host = DB_HOST;
     private $user = DB_USER;
@@ -14,16 +14,16 @@ class Database
     {
         //data source name 
         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
-        
+
         $option = [
-            PDO::ATTR_PRESISTENT => true,
+            PDO::ATTR_PERSISTENT => true,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ];
 
 
         try {
             $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             die($e->getMessage());
         }
     }
@@ -33,52 +33,46 @@ class Database
         $this->stmt = $this->dbh->prepare($query);
     }
 
-    public function bind($param, $value, $type=null)
+    public function bind($param, $value, $type = null)
     {
-        if( is_null($type) )
-        {
-            switch(true)
-            {
-                case is_int($value) : 
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
                     $type = PDO::PARAM_INT;
                     break;
-                case is_bool($value) :
+                case is_bool($value):
                     $type = PDO::PARAM_BOOL;
-                    break;    
-                case is_null($value) :
+                    break;
+                case is_null($value):
                     $type = PDO::PARAM_STR;
                     break;
-                default : 
+                default:
                     $type = PDO::PARAM_STR;
             }
         }
 
-        $this -> stmt -> bindValue($param, $value, $type);
-
-
+        $this->stmt->bindValue($param, $value, $type);
     }
 
     public function execute()
     {
-        $this -> stmt -> execute();
-
+        $this->stmt->execute();
     }
 
     public function resultSet()
     {
-        $this -> execute();
+        $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function single()
     {
-        $this -> execute();
+        $this->execute();
         return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public function rowCount()
     {
-        return $this -> stmt -> rowCount();
+        return $this->stmt->rowCount();
     }
-
 }
