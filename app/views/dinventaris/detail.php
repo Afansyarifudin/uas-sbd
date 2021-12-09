265571a161746e6ca1f73b0a74c730ce9e4c53e6
Fidyastya <fidyastya932@gmail.com>
2021-12-09 10:31:00 +0700

    view detail inventaris

---
+++ app/views/dinventaris/detail.php
@@ -0,0 +1,87 @@
+<!-- Content -->
+
+<div class="container-fluid">
+    <div class="d-sm-flex justify-content-between align-items-center mb-4">
+        <h3 class="text-dark mb-0">Detail Peminjaman</h3>
+    </div>
+
+    <!-- kontennya page disini -->
+    <div class="card shadow">
+        <div class="card-header py-3">
+            <p class="text-primary m-0 font-weight-bold">Detail Peminjaman</p>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-4 text-nowrap">
+                    <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable"><label>Show&nbsp;<select class="form-control form-control-sm custom-select custom-select-sm">
+                                <option value="10" selected="">10</option>
+                                <option value="25">25</option>
+                                <option value="50">50</option>
+                                <option value="100">100</option>
+                            </select>&nbsp;</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                </div>
+                <div class="col-md-2">
+
+                </div>
+            </div>
+            <!-- Tabel -->
+            <div class="table-responsive mt-2">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr class="text-center">
+                            <th>Nama Inventaris</th>
+                            <th>Nama Gedung</th>
+                            <th>Nama Ruangan</th>
+                            <th>Nama Barang</th>
+                            <th>Tanggal Inventaris</th>
+                            <th>Keterangan Inventaris</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Inventaris Ruang Jarkom</td>
+                            <td>D2</td>
+                            <td>R110</td>
+                            <td>Meja</td>
+                            <td>12/21/2021</td>
+                            <td>Laboratorium Jarkom</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 align-self-center">
+                    <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Showing 1 to 10 of 27</p>
+                </div>
+                <div class="col-md-6">
+                    <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
+                        <ul class="pagination">
+                            <li class="page-item disabled"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<!-- div ini jangan di hapus ya karena ada sambungannya sama yg atas -->
+</div>
+
+<!-- footer -->
+<footer class="bg-white sticky-footer">
+    <div class="container my-auto">
+        <div class="text-center my-auto copyright"><span>Copyright © kelompok 6</span></div>
+    </div>
+</footer>
+</div><a class="border rounded d-inline scroll-to-top" href="<?= BASEURL; ?>/peminjaman"><i class="fas fa-angle-up"></i></a>
+</div>
